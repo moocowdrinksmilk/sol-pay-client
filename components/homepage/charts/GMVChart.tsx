aa675fa8911cafa63124f54bc1f64e2942c49849
Rui Xian <tayrx9988@gmail.com>
2022-05-29 15:12:10 +0800

    feat: put graph placeholders

---
+++ components/homepage/charts/GMVChart.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, TooltipProps } from 'recharts';
+import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+const handleToolTip = (props: TooltipProps<ValueType, NameType>) => {
+    return (
+        <div>
+            {}
+        </div>
+    )
+}
+
+const GMVChart = () => {
+    return (
+        <ResponsiveContainer width="100%" height="100%" >
+            <AreaChart
+                data={data}
+                margin={{
+                    top: 50,
+                    right: 0,
+                    left: 0,
+                    bottom: 0,
+                }}
+            >
+                <Tooltip content={handleToolTip} />
+                <Area type="monotone" stroke="#E9F2FE" dataKey="uv" fill="#EDF5FF" dot={false} opacity={1} />
+                <Area type="monotone" stroke="none" dataKey="pv" fill="#006DE3" dot={false} opacity={0.5} />
+            </AreaChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default GMVChart
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,7 @@
+import GMVChart from "../components/homepage/charts/GMVChart"
 import SideBar from "../components/homepage/Sidebar"
+import { VscTriangleUp } from 'react-icons/vsc'
+import { AiOutlineEllipsis } from 'react-icons/ai'
 
 const Dashboard = () => {
     return (
@@ -10,7 +13,7 @@ const Dashboard = () => {
 
                 </div>
 
-                <div className="col w-full px-6 pt-4 gap-4">
+                <div className="col w-full px-6 pt-4 gap-4 h-full">
                     <div>
                         <h1 className="text-2xl font-bold">
                             Welcome back Wei Zhen
@@ -21,8 +24,56 @@ const Dashboard = () => {
                         </h2>
                     </div>
 
-                    <div className="row rounded-md h-20 bg-white">
+                    <div className="row h-full">
+                        <div className="col w-9/12 pb-10 gap-2">
 
+                            <div className="col rounded-md h-1/3 bg-white relative z-10 relative">
+                                <div className="absolute top-0 left-0 p-4">
+                                    <h1>
+                                        GMV
+                                    </h1>
+
+                                    <div className="row items-center">
+                                        <VscTriangleUp fill="green" />
+                                        <span>
+                                            12.2%
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div className="absolute top-0 right-0 p-4">
+                                    <AiOutlineEllipsis />
+                                </div>
+                                <div className="h-full rounded-md">
+                                    <GMVChart />
+                                </div>
+                            </div>
+
+                            <div className="row h-1/3 gap-2">
+                                <div className="flex-1 rounded-md bg-white">
+
+                                </div>
+
+                                <div className="flex-1 rounded-md bg-white">
+
+                                </div>
+                            </div>
+
+                            <div className="row h-1/3 gap-2">
+                                <div className="flex-1 rounded-md bg-white">
+
+                                </div>
+
+                                <div className="flex-1 rounded-md bg-white">
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="">
+
+                        </div>
                     </div>
                 </div>
             </div>
